992d8795b78faa15c389c9cfcf5af04303ba1365
rzadp <roopert7@gmail.com>
2023-11-23 10:28:53 +0100

    First try

---
+++ mdbook/book.sh
@@ -0,0 +1,37 @@
+#/usr/bin/env bash
+set -euo pipefail
+cd $(dirname ${BASH_SOURCE[0]})
+
+mkdir -p src/{approved,proposed}
+cat SUMMARY_preface.md > src/SUMMARY.md
+
+cp ../../RFCs/text/*.md src/approved/
+
+append_rfc_to_summary () {
+  local file="$1"
+  local title=$(head -n 1 $file) # Grab the title from the contents of the file
+  local title=${title#\# } # Remove the "#: " prefix
+  local path=${file#./src/} # Relative path, without the src prefix (format required by mdbook)
+  echo "- [$title]($path)" >> src/SUMMARY.md;
+}
+
+for f in ./src/approved/*.md;
+do
+  append_rfc_to_summary "$f"
+done
+
+echo "\n---\n\n# Proposed\n\n" >> src/SUMMARY.md
+
+for f in ./src/proposed/*.md;
+do
+  [ -e "$f" ] || break
+  append_rfc_to_summary "$f"
+done
+
+cat SUMMARY_postface.md >> src/SUMMARY.md
+
+echo "Preview of the generated SUMMARY.md:\n"
+cat src/SUMMARY.md
+
+rm -rf ../book/
+mdbook build --dest-dir ../book/
